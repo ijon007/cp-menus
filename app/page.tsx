0dd251cdf88b87eee64bb0c1423753491b2ccaa0
codify4 <kushta.joni@gmail.com>
2025-12-13 08:31:58 +0100

    project reinit

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,5 @@
-import Link from "next/link"
+import { ComponentExample } from "@/components/component-example";
 
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-
-export default function Home() {
-  return (
-    <div className="min-h-svh bg-background text-foreground flex items-center justify-center p-6">
-      <Card className="w-full max-w-2xl">
-        <CardHeader>
-          <CardTitle className="text-2xl">cp-menus</CardTitle>
-          <CardDescription>
-            Restaurants create menus in admin, customers view them on a public route.
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="flex flex-col gap-3 sm:flex-row">
-          <Button asChild className="w-full">
-            <Link href="/login">Login</Link>
-          </Button>
-        </CardContent>
-      </Card>
-    </div>
-  )
+export default function Page() {
+return <ComponentExample />;
 }
